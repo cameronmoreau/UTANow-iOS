b7e518b8b3ca68ceffb54c69378468cf99f4b0ae
Cameron Moreau <Cameron@Camerons-MacBook-Pro.local>
2015-11-07 10:31:28 -0600

    Initial Commit with Cocoa Pods, Mapbox and a few other features

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'UTANow' do
+  pod 'Kingfisher', '~> 1.7'
+  pod 'Mapbox-iOS-SDK', '~> 2.1.2'
+end
---
+++ UTANow/UTANow-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  UTANow-Bridging-Header.h
+//  UTANow
+//
+//  Created by Cameron Moreau on 11/6/15.
+//  Copyright © 2015 Mobi. All rights reserved.
+//
+
